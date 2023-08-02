b19838d37db33cbf1084d5b20eeb8ad83476abb6
GabriFili850 <gabriele.filigheddu@live.it>
2023-08-02 12:16:59 +0100

    adds a new example field to add richText

--- src/templates/blogPost.tsx
+++ src/templates/blogPost.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { GatsbyImage, IGatsbyImageData, getImage } from "gatsby-plugin-image"
+import { BLOCKS, INLINES } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { StyledBlogPostTitle, Content } from "./styles"
 
 export const query = graphql`
@@ -9,6 +11,9 @@ export const query = graphql`
       title
       content {
         content
+      }
+      contentWithRichtext {
+        raw
       }
       image {
         gatsbyImageData(
@@ -29,6 +34,9 @@ interface BlogPostProps {
       content: {
         content: string
       }
+      contentWithRichtext: {
+        raw: string
+      }
       image: {
         gatsbyImageData: IGatsbyImageData
       }
@@ -37,14 +45,36 @@ interface BlogPostProps {
 }
 
 const BlogPost: React.FC<BlogPostProps> = ({ data }) => {
+  const options = {
+    renderNode: {
+      [INLINES.HYPERLINK]: node => {
+        if (node.content[0]) {
+          return (
+            <a href={node.data.uri} target="_blank" rel="noreferrer">
+              {node.content[0].value}
+            </a>
+          )
+        }
+        return null
+      },
+    },
+  }
+
+  const imageData = getImage(data.contentfulBlogPost.image.gatsbyImageData)
+
   return (
     <div>
       <StyledBlogPostTitle>{data.contentfulBlogPost.title}</StyledBlogPostTitle>
-      <GatsbyImage
-        image={getImage(data.contentfulBlogPost.image) as IGatsbyImageData}
-        alt={data.contentfulBlogPost.title}
-      />
-      <Content>{data.contentfulBlogPost.content.content}</Content>
+      {imageData && (
+        <GatsbyImage image={imageData} alt={data.contentfulBlogPost.title} />
+      )}
+      {data.contentfulBlogPost.content && (
+        <Content>{data.contentfulBlogPost.content.content}</Content>
+      )}
+      {documentToReactComponents(
+        JSON.parse(data.contentfulBlogPost.contentWithRichtext.raw),
+        options
+      )}
     </div>
   )
 }
