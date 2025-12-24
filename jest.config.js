7654062355321fe0cde0448294b6cce94c4ee8e8
GabriFili850 <gabriele.filigheddu@live.it>
2025-12-24 23:48:58 +0000

    add test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
--- src/templates/blogPost.test.tsx
+++ src/templates/blogPost.test.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
+import { ThemeProvider } from "styled-components"
+import { theme } from "../styles/theme"
 import BlogPost from "./blogPost"
 
 const mockData = {
@@ -16,7 +18,11 @@ const mockData = {
 
 describe("BlogPost template", () => {
   it("renders the post title and content", () => {
-    render(<BlogPost data={mockData} />)
+    render(
+      <ThemeProvider theme={theme}>
+        <BlogPost data={mockData} />
+      </ThemeProvider>
+    )
     expect(screen.getByText("Hello world")).toBeTruthy()
     expect(screen.getByText("Plain content")).toBeTruthy()
   })
